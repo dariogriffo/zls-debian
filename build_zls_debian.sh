2d29dccc2343b6a5e00d85c1824f49c6622d0f15
Dario Griffo <dariogriffo@gmail.com>
2026-03-22 11:34:54 +0000

    Improve build using already built binaries

--- build_zls_debian.sh
+++ build_zls_debian.sh
@@ -1,19 +1,43 @@
 ZIG_VERSION=$1
 BUILD_VERSION=$2
+
+UNAME_ARCH=$(uname -m)
+case "$UNAME_ARCH" in
+  x86_64)      ZLS_ARCH="x86_64-linux";      DEB_ARCH="amd64"   ;;
+  aarch64)     ZLS_ARCH="aarch64-linux";     DEB_ARCH="arm64"   ;;
+  armv7l)      ZLS_ARCH="arm-linux";         DEB_ARCH="armhf"   ;;
+  i686)        ZLS_ARCH="x86-linux";         DEB_ARCH="i386"    ;;
+  ppc64le)     ZLS_ARCH="powerpc64le-linux"; DEB_ARCH="ppc64el" ;;
+  riscv64)     ZLS_ARCH="riscv64-linux";     DEB_ARCH="riscv64" ;;
+  s390x)       ZLS_ARCH="s390x-linux";       DEB_ARCH="s390x"   ;;
+  loongarch64) ZLS_ARCH="loongarch64-linux"; DEB_ARCH="loong64" ;;
+  *) echo "Unsupported architecture: $UNAME_ARCH"; exit 1 ;;
+esac
+
+mkdir zls_download
+cd zls_download
+wget https://github.com/zigtools/zls/releases/download/$ZIG_VERSION/zls-$ZLS_ARCH.tar.xz
+tar -xf zls-$ZLS_ARCH.tar.xz
+cd ..
+mv zls_download/zls .
+
 declare -a arr=("bookworm" "trixie" "forky" "sid")
 for i in "${arr[@]}"
 do
   DEBIAN_DIST=$i
-  FULL_VERSION=$ZIG_VERSION-${BUILD_VERSION}+${DEBIAN_DIST}_amd64
-  
-  docker build . -t zls-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION -f meta_Dockerfile
+  FULL_VERSION=$ZIG_VERSION-${BUILD_VERSION}+${DEBIAN_DIST}_${DEB_ARCH}
+
+  docker build . -t zls-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION --build-arg DEB_ARCH=$DEB_ARCH -f meta_Dockerfile
   id="$(docker create zls-$DEBIAN_DIST)"
   docker cp $id:/zls_$FULL_VERSION.deb - > ./zls_$FULL_VERSION.deb
   tar -xf ./zls_$FULL_VERSION.deb
 
-  docker build . -t zls-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION -f zero_Dockerfile
+  docker build . -t zls-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION --build-arg DEB_ARCH=$DEB_ARCH -f zero_Dockerfile
   id="$(docker create zls-$DEBIAN_DIST)"
   docker cp $id:/zls-zero_$FULL_VERSION.deb - > ./zls-zero_$FULL_VERSION.deb
   tar -xf ./zls-zero_$FULL_VERSION.deb
 
 done
+
+rm -fRd zls_download
+rm -f zls
